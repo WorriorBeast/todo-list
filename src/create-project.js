74838e1ee0267c65d056f54426cd4985d0c3bd7b
Emmanuel Sanchez <128549074+WorriorBeast@users.noreply.github.com>
2024-06-12 17:49:35 -0700

    Isolate function to allow reusability

--- src/create-project.js
+++ src/create-project.js
@@ -1,20 +1,6 @@
-import { default as SideBarProjectTab } from "./sidebar-project-tab";
-import { Form, Header, Description, Checklist, Notes, Submit, Priority } from "./project-form";
-
-const createProjectInstances = [
-   new SideBarProjectTab(),
-   new Form(),
-   new Header(),
-   new Description(),
-   new Checklist(),
-   new Notes(),
-   new Priority,
-   new Submit()
-];
-
-export function createProject() {
+export function createProject(createProjectInstances) {
    for (let i = 0; i < createProjectInstances.length; i++) {
       Object.getOwnPropertyNames(Object.getPrototypeOf(createProjectInstances[i]))
       .filter(methodName => (methodName !== 'constructor')).map(method => createProjectInstances[i][method]());
    }
-}
+};
