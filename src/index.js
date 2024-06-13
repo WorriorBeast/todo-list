431d01c0c8c9931cab38c9e99b38303378014db2
Emmanuel Sanchez <128549074+WorriorBeast@users.noreply.github.com>
2024-06-12 17:59:44 -0700

    Create array local to file and edit event function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import './style.css';
 import { createProject } from './create-project.js';
+import { default as SideBarProjectTab } from "./sidebar-project-tab";
+import { Form, Header, Description, Checklist, Notes, Submit, Priority } from "./project-form";
+
+const createTabAndForm = [
+   new SideBarProjectTab(),
+   new Form(),
+   new Header(),
+   new Description(),
+   new Checklist(),
+   new Notes(),
+   new Priority,
+   new Submit()
+];
 
 const addProjectBtn = document.querySelector('.add-icon-button');
 
-addProjectBtn.addEventListener('click', createProject);
+addProjectBtn.addEventListener('click', function() {
+   createProject(createTabAndForm);
+});
