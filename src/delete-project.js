6800917be3fac688859f27e9668dfde85f463e01
Emmanuel Sanchez <128549074+WorriorBeast@users.noreply.github.com>
2024-06-29 19:45:26 -0700

    Delete saved project and associated elements

--- src/delete-project.js
+++ src/delete-project.js
@@ -1,9 +1,27 @@
-export default function() {
-	const content = document.getElementById('content');
-	const form = document.getElementById('create-project');
+import { default as removeCurrentContent } from './remove-current-content.js';
+
+export default function(e) {
+	e.stopPropagation();
+	removeCurrentContent();
+
 	const ul = document.querySelector('.project-list');
 	let currentProjectTab = this.closest('.project-item');
 
+	let currentName = currentProjectTab.children[0].textContent;
+	let currentDueDate = currentProjectTab.children[1].textContent.split(' ');
+
+	let savedProjects = JSON.parse(localStorage.getItem('projects'));
+
+	for (let project in savedProjects) {
+		let savedName = savedProjects[project].name;
+		let savedDueDate = savedProjects[project].dueDate;
+
+		if (Object.is(savedName, currentName) && Object.is(savedDueDate, currentDueDate[1])) {
+			const {[project]: remove , ...projects} = savedProjects;
+			
+			localStorage.setItem('projects', JSON.stringify(projects));
+		}
+	}
+
 	ul.removeChild(currentProjectTab);
-	content.removeChild(form);
 };
